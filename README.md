2e8dd3de83de7a9a12a90e2c69a2872f0952fd0f
Shaurya Chanana <15051933+chanana@users.noreply.github.com>
2021-08-16 13:31:24 -0400

    Account for files with no data values

--- README.md
+++ README.md
@@ -12,8 +12,15 @@ pip install uv2json
 
 ## Usage
 
+#### As a console script
 ```bash
-$ uv2json <path/to/csv/files>
+uv2json <path/to/csv/files>
+```
+
+#### As a module
+```python
+from uv2json import uv2json as uv
+uv.convert('path/to/filename') # Accepts single file or list of files.
 ```
 
 The script will make a JSON file in the same location with the same name.
--- uv2json/uv2json.py
+++ uv2json/uv2json.py
@@ -1,14 +1,18 @@
 from json import dump
 from io import StringIO
+from typing import List
 from pandas import read_csv
 
 
-def convert(filenames):
+def convert(filenames: List[str]) -> None:
     """convert files from csv to json
 
     Args:
-        filenames (List[str]): list of filenames with path. each path must be a string
+        filenames (List[str]): list of filenames with path; each path must be a string
     """
+    if not type(filenames) == list:
+        filenames = [filenames]
+
     for filename in filenames:
         # split the file based on dashes; use two dashes since negative numbers also contain dashes
         with open(filename, "r") as infile:
@@ -22,21 +26,26 @@ def convert(filenames):
         sample_info = list(
             filter(None, data[1].strip("$").split("\n"))
         )  # second item in list contains sample information
-
         # assemble the dictionary
         sample_dict = {}
         for item in sample_info:
             intensity = item.strip("$").split(":", 1)
             sample_dict[intensity[0]] = intensity[1]
 
+        if "No data" in data[-1]:
+            print("blank file")
+            continue
+        else:
+            print(sample_dict)
+
         # read the last item of the data list as a dataframe
         dataFrame = read_csv(
             StringIO(data[-1]), delimiter="\t", names=["254", "280", "320", "FC"]
         )
+
         # expected format for each column is one of the following:
         # <intensity,time> or <intensity>
 
-        # print(df.info())
         # if <intensity>
         sample_dict["time"] = [i / 10 for i in range(0, dataFrame.shape[0])]
 
